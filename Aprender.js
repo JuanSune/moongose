9e2aae398e1109029774d55da12a4acdcf72dec0
Juan Suñe <111535673+JuanSune@users.noreply.github.com>
2023-01-06 18:42:52 -0300

    up

--- Aprender.js
+++ Aprender.js
@@ -1,8 +1,43 @@
-const mongoose = require('mongoose');
+const { default: mongoose } = require('mongoose');
+const mong = require('mongoose');
 
 // Configurando o Mongoose
-mongoose.connect('mongodb://0.0.0.0:27017/aprendendo').then(()=> {
+mong.connect('mongodb://0.0.0.0:27017/aprendendo', { useNewUrlParser: true }).then(()=> {
     console.log('MongoDB conectado')
-}).catch((err)=> {
-    console.log('Erro ao se conectar: ' + err)
+}).catch((erro)=> {
+    console.log('Erro ao se conectar: ' + erro)
 })
+
+const UserSchema = mong.Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    sobrenome: {
+        type: String,
+        required: true
+    },
+    idade: {
+        type: Number,
+        required: true
+    },
+    pais: {
+        type: String
+    }
+});
+
+// Collection
+mong.model('usuarios', UserSchema);
+
+const vik = mong.model('usuarios');
+
+new vik({
+    nome: "vik",
+    sobrenome: "lex",
+    idade: 12,
+    pais:"gtland"
+}).save().then(()=>{
+    console.log("Cadastrado com sucesso!");
+}).catch((err)=>{
+    console.log("Infelizmente ocorreu o seguinte erro "+err);
+});
